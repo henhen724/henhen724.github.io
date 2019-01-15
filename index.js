46f5d5a8edc127f39bcd143cb2f2dcb1fa368c76
Henry S Hunt <henhen7.24@gmail.com>
2019-01-14 20:06:19 -0600

    style fixed

---
+++ index.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    var url = document.location.href;
+    try{
+        params = url.split('?')[1].split('&'),
+        data = {}, tmp;
+        for (var i = 0, l = params.length; i < l; i++) {
+          tmp = params[i].split('=');
+          data[tmp[0]] = tmp[1];
+        }
+        document.getElementById('here').innerHTML = data.name;
+    }
+    catch(err) {
+      if(err.message === "Cannot read property 'split' of undefined"){
+        console.log('No query')
+      }
+      else {
+        console.log(err.message)
+      }
+    }
+}
+
+function searchAttempt() {
+    var input = document.getElementById('header-search-bar').value;
+    //window.history.replaceState(statedata, title, url);
+    console.log(input);
+}
+Scout.configure({ clientId: "eb415df5-c945-4ffa-a94e-4d6b8d4dc315" }).then(() => {});
+Scout.titles.list().then(titles => console.log(titles));
