d4119f55543c6bf829f495a8c97244be9c980393
Henry S Hunt <henhen7.24@gmail.com>
2019-01-23 01:32:43 -0600

    Post-Login Work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const { ensureAuthenticated } = require('../config/auth')
 
 router.get('/', (req, res) => res.render('welcome'));
 
+router.get('/about', (req, res) => res.render('about'));
+
 router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
   name: req.user.name
 }));
