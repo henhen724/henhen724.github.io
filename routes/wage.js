c3104b428c7ed5f85641a6f3ff2920e5f5fd6208
Henry Hunt <henhen7.24@gmail.com>
2024-07-21 17:51:19 -0700

    cleaning house

--- routes/wage.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { ensureAuthenticated } = require('../config/auth')
-
-router.get('/place', ensureAuthenticated, (req, res) => res.render('place'))
-
-router.get('/addroyal', ensureAuthenticated, (req, res) => res.render('addroyal'))
-
-module.exports = router
